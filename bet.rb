079261ce733a1ea1272e543a6635b7a20e504299
won <wjin@nyit.edu>
2014-10-18 17:56:01 -0400

    ruby logic in route handler em

---
+++ bet.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/bet/:stake/on/:number' do
+	stake = params[:stake].to_i
+	number = params[:number].to_i
+	roll = rand(6)+1
+	if number == roll
+		"It landed on #{roll}.  Well done, you win #{6*stake} chips"
+	else
+		"It landed on #{roll}.  you lose your stake of #{stake} chips"
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- hello.rb
+++ hello.rb
@@ -11,6 +11,29 @@ require 'sinatra/reloader' if development?
 		"Hello #{name}"
 	end
 
+	# This route contains a named parameter called :name signified by : colon
+	# Named parameters can go anywhere on a URL, and available in the
+	# handler as part of the params hash.(hash uses key/value pair system for data storage)
+	# params hash is automatically created to hold into entered as named 
+	# parameters on the URL or as paramenter sent via HTML form, great way
+	# to grab informatoin from a URL
+	get '/:name' do
+		name = params[:name]
+		"Hi there #{name}!"
+	end
+
+	get '/:one/:two/:three' do
+		"first: #{params[:one]}, second: #{params[:two]}, third: #{params[:three]}"
+	end
+
+	get '/what/time/is/it/in/:number/hours' do
+		number = params[:number].to_i
+		time = Time.now + number * 3600
+		"The Time in #{number} hours will be #{time.strftime('%I:%M %P')}"
+	end
+
+
+
 
 
 
