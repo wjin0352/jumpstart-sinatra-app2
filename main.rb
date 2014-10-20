b6ee4192cd0ffec3fb3edef41ff8a1f21bf52cb1
won <wjin@nyit.edu>
2014-10-20 14:08:59 -0400

    basic sessions and session based authentication

--- main.rb
+++ main.rb
@@ -4,6 +4,13 @@ require 'sass'
 require 'slim'
 require './song'
 
+configure do
+	enable :sessions
+	set :username, 'frank'
+	set :password, 'sinatra'
+end
+
+
 get('/styles.css'){ scss :styles }
 
 get '/' do
@@ -26,6 +33,42 @@ not_found do
   slim :not_found
 end
 
+# this route grabs name entered in URL and stores it in params hash, problem is the info
+# in the params hash will be available only for that request, so we store it in session hash
+# so it is available for all requests.
+get '/set/:name' do
+	session[:name] = params[:name]
+end
+
+get '/get/hello' do
+	"Hello #{session[:name]}"
+end
+
+# we enabled sessions in the config block above, now we need a login route handler that 
+# displays a view called login that contains a form to be submitted.
+get '/login' do
+	slim :login
+end
+
+# we need to create a handler to deal with a form being submitted from above
+post '/login' do
+	if params[:username]  == settings.username && params[:password] == settings.password
+		session[:admin] = true
+		redirect to('/songs')
+	else
+		slim :login
+	end
+end
+
+get '/logout' do
+	session.clear
+	redirect to('/login')
+end
+
+
+
+
+
 
 
 
@@ -33,4 +76,24 @@ end
 # set :public_folder, 'assets'
 
 # set :views, 'templates'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- song.rb
+++ song.rb
@@ -2,14 +2,18 @@ require 'dm-core'
 require 'dm-migrations'
 
 
-configure do
+configure :development do
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 # this creates a file called development.db (if it doesnt already exist)
 # which stores all database information, now lets create our song class
 
 # the include DataMapper::Resource line is how we make any ruby class
 # a DataMapper resource.
+
+# Dont worry where these configure blocks go, they can go anywhere and have multiples
+# the convention is to have one config block thats near top of the file
 end
+
 
 class Song
 	include DataMapper::Resource
