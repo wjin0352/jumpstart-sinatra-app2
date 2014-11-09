9f62103e32d735903d4c35a897a1053dd868daac
won <wjin@nyit.edu>
2014-11-09 17:25:03 -0500

    added like button with ajax

--- song.rb
+++ song.rb
@@ -20,6 +20,7 @@ class Song
 	property :lyrics, Text
 	property :length, Integer
 	property :released_on, Date 
+	property :likes, Integer, :default => 0
 
 	def released_on=date
     super Date.strptime(date, '%m/%d/%Y')
@@ -120,6 +121,14 @@ end
 delete '/songs/:id' do
 	find_song.destroy
 	redirect to('/songs')
+end
+
+post '/songs/:id/like' do
+	@song = find_song
+	@song.likes = @song.likes.next
+	@song.save
+	redirect to"/songs/#{@song.id}" unless request.xhr?
+	slim :like, :layout => false
 end
 
 # post '/contact' do
